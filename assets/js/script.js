87e211bf9be87f22e495de095bc1b9c9731f5969
thisisjona <thisisjona@gmail.com>
2022-02-01 19:51:20 -0600

    added submit even listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,16 @@
-var buttonEL = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function(){
+var createTaskHandler = function(event){
+
+    event.preventDefault();
+
     var listItemEL = document.createElement("li");
     listItemEL.className = "task-item";
-    listItemEL.textContent = "This is a new task.";
-    tasksToDoEl.appendChild(listItemEL)
+    listItemEL.textContent = "This is a new Task";
+    tasksToDoEl.appendChild(listItemEL);
+     
+    console.log(event);
 };
 
-buttonEL.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
