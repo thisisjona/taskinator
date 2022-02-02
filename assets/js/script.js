c4f13ea264e88e5052d7a175300d28f1d9b07329
thisisjona <thisisjona@gmail.com>
2022-02-01 22:46:42 -0600

    added error handling/addressed usability concerns

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,14 @@ var taskFormHandler = function(event){
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
     
+    if(!taskNameInput || !taskTypeInput){
+        alert("You need to fill out the task form!");
+        return false;
+    };
+
+    //reset the form
+    formEl.reset();
+    
     //package up data as an object
     var taskDataObj = {
         name : taskNameInput,
