efb58fe4488e0f3e7ba4442a1a67aa54ad62f399
thisisjona <thisisjona@gmail.com>
2022-02-01 21:23:35 -0600

    Capture Form Field Values

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,12 +5,23 @@ var createTaskHandler = function(event){
 
     event.preventDefault();
 
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    
+    //create a list item
     var listItemEL = document.createElement("li");
     listItemEL.className = "task-item";
-    listItemEL.textContent = "This is a new Task";
+
+    //create a div to hold task info and add to list item & give it a class name
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+
+    //add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    listItemEL.appendChild(taskInfoEl);
+
+    //add entire list item to list
     tasksToDoEl.appendChild(listItemEL);
-     
-    console.log(event);
 };
 
 formEl.addEventListener("submit", createTaskHandler);
